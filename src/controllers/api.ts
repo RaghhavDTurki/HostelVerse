bf5b6548e705b886018e628222fdf4261a901773
RaghhavDTurki <raghhavdturki@gmail.com>
2022-01-21 17:46:29 +0530

    (chore): remove unwanted lines from template

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,15 +1,14 @@
 "use strict";
 
-import graph from "fbgraph";
-import { Response, Request, NextFunction } from "express";
-import { UserDocument } from "../models/User";
+import { Response, Request } from "express";
+// import { UserDocument } from "../models/User";
 
 
 /**
  * List of API examples.
  * @route GET /api
  */
-export const getApi = (req: Request, res: Response) => {
+export const getApi = (req: Request, res: Response): void => {
     res.render("api/index", {
         title: "API Examples"
     });
@@ -19,15 +18,15 @@ export const getApi = (req: Request, res: Response) => {
  * Facebook API example.
  * @route GET /api/facebook
  */
-export const getFacebook = (req: Request, res: Response, next: NextFunction) => {
-    const user = req.user as UserDocument;
-    const token = user.tokens.find((token: any) => token.kind === "facebook");
-    graph.setAccessToken(token.accessToken);
-    graph.get(`${user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-        if (err) { return next(err); }
-        res.render("api/facebook", {
-            title: "Facebook API",
-            profile: results
-        });
-    });
-};
+// export const getFacebook = (req: Request, res: Response, next: NextFunction) => {
+//     const user = req.user as UserDocument;
+//     const token = user.tokens.find((token: any) => token.kind === "facebook");
+//     graph.setAccessToken(token.accessToken);
+//     graph.get(`${user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
+//         if (err) { return next(err); }
+//         res.render("api/facebook", {
+//             title: "Facebook API",
+//             profile: results
+//         });
+//     });
+// };

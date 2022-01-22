028f1880576cb9f57e32444d8479e4372a8de827
RaghhavDTurki <raghhavdturki@gmail.com>
2022-01-23 02:07:14 +0530

    (fix): minor bugs

--- src/routes/student/roomIssue.ts
+++ src/routes/student/roomIssue.ts
@@ -2,7 +2,9 @@ import { Request, Response } from "express";
 import { RoomIssue } from "../../models/RoomIssue";
 
 export async function createRoomIssue(req: Request, res: Response): Promise<void> {
-
+    if(!req.body){
+        res.status(400).send({ message : "Body cannot be empty!"});
+    }
     const roomissue = new RoomIssue();
     roomissue.hostelid = req.body.hostelid;
     roomissue.roomno = req.body.roomno;
@@ -10,6 +12,6 @@ export async function createRoomIssue(req: Request, res: Response): Promise<void
 
     roomissue.save()
         .then(data => {
-            res.send("Room issue created successfully.")
-        }).catch(err => res.status(500).send({ message: err.message || "An error occured while creating a room issue."}))
+            res.send("Room issue created successfully.");
+        }).catch(err => res.status(500).send({ message: err.message || "An error occured while creating a room issue."}));
 }

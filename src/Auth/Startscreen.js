1a9d1a3adeb946332ef9fb9864ae0f0f7ef9080b
Akash Tureha <2020kucp1138@iiitkota.ac.in>
2022-01-22 15:37:02 +0530

    Added Screens

---
+++ src/Auth/Startscreen.js
@@ -0,0 +1,70 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+
+import {width, colors, height} from '.././utils/constants';
+import {background} from '../../assets/index';
+
+const Startscreen = ({navigation}) => {
+  return (
+    <SafeAreaView style={{backgroundColor: 'white', flex: 1}}>
+      <ImageBackground
+        source={background}
+        resizeMode="cover"
+        style={{
+          backgroundColor: colors.yellow,
+          flex: 1,
+          alignItems: 'center',
+          position: 'relative',
+        }}>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: height * 0.05,
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: width * 0.05,
+              fontWeight: '500',
+              color: colors.black,
+            }}>
+            Enter into the magical world of
+          </Text>
+          <Text
+            style={{
+              fontSize: width * 0.1,
+              fontWeight: '500',
+              color: colors.black,
+            }}>
+            {'HostelVerse'.toUpperCase()}
+          </Text>
+          <Text style={{fontSize: width * 0.04, textAlign: 'center'}}>
+            We have created an awesome app to tackle the problems related to
+            hostels. Tighten your seat belt to go into a fascinating journey.
+          </Text>
+          <TouchableOpacity
+            style={{
+              backgroundColor: colors.black,
+              width: width * 0.4,
+              height: height * 0.04,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: width * 0.01,
+              marginTop: height * 0.015,
+            }}
+            onPress={() => navigation.navigate('Filter')}>
+            <Text style={{color: colors.white}}>Get Started</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
+
+export default Startscreen;
---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+import {Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('window');
+
+const colors = {
+  yellow: '#FCD129',
+  white: 'white',
+  black: 'black',
+};
+
+export {width, height, colors};
